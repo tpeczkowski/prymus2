d309d1d3c508505cac0eeec628cf24f3e44b28e9
Student <student@d17-l427-pc-15.ipa.ki.agh.edu.pl>
2019-11-22 17:38:20 +0100

    Vue.js 1

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    Firstname <input type="text" v-model="Firstname">
+    <br>
+    Lastname <input type="text" v-model="Lastname">
+    <br>
+    <button @click="addNew()">Add</button>
+  <ul>
+    <li v-for="element in numbers">{{ element }}</li>
+  </ul>
+    
+
+
+  </div>
+</template>
+
+<script>
+export default {
+        data() {
+            return {numbers: []}
+        },
+        methods: {
+            addNew() {
+              
+                this.numbers.push(this.Firstname+' '+this.Lastname)
+              }
+            }
+        };
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#app > ul
+{
+  list-style-type: none;
+}
+</style>
